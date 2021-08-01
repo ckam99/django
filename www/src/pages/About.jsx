4b1f4aad16cd4a22d38e702b1567f3f45f162d5d
Ckam <ckam225@gmail.com>
2021-08-01 07:30:46 -0700

    django react nginx docker

---
+++ www/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return <div>
+        <h1>About us</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe perferendis neque non quasi commodi ratione impedit quis mollitia. Fuga, dignissimos consequatur? Beatae asperiores rem ab amet totam fuga hic animi.
+        </p>
+    </div>
+}
+
+export default About;
---
+++ www/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+    return <div>
+        <h1>Welcome</h1>
+    </div>
+}
+
+export default Home;
