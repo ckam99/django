dbc38ad1a4d1a106ab1b06af9acf7eb2bca759b5
Ckam <ckam225@gmail.com>
2021-08-01 05:42:56 -0700

    added prod and dev build

---
+++ src/core/celery.py
@@ -0,0 +1,11 @@
+import os
+
+from celery import Celery
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+app = Celery('core')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
--- src/core/settings.py
+++ src/core/settings.py
@@ -136,7 +136,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'base/static'
+# ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
