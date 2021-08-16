a6158d497400b90e74c3bfd955cd9ce5bd4235ea
msi89 <claver.developper@gmail.com>
2021-08-17 02:36:32 +0300

    setup login

--- src/base/serializers.py
+++ src/base/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from django.contrib.auth import authenticate
 from .models import User
 
 
@@ -18,3 +18,37 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.CharField(max_length=255)
+    username = serializers.CharField(max_length=255, read_only=True)
+    password = serializers.CharField(max_length=128, write_only=True)
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    def validate(self, data):
+        email = data.get('email', None)
+        password = data.get('password', None)
+
+        if email is None:
+            raise serializers.ValidationError(
+                'An email address is required to log in.'
+            )
+        if password is None:
+            raise serializers.ValidationError(
+                'A password is required to log in.'
+            )
+        user = authenticate(username=email, password=password)
+        if user is None:
+            raise serializers.ValidationError(
+                'A user with this email and password was not found.'
+            )
+        if not user.is_active:
+            raise serializers.ValidationError(
+                'This user has been deactivated.'
+            )
+        return {
+            'email': user.email,
+            'username': user.username,
+            'token': user.token
+        }
--- src/base/urls.py
+++ src/base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import RegistrationAPIView
+from .views import RegistrationAPIView, LoginAPIView
 
 app_name = 'base'
 urlpatterns = [
-    path('users/', RegistrationAPIView.as_view()),
+    path('auth/register/', RegistrationAPIView.as_view()),
+    path('auth/login/', LoginAPIView.as_view()),
 ]
