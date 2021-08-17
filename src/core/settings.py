4a3dac3db91319ed685de3a4219e257e7e1299ff
msi89 <claver.developper@gmail.com>
2021-08-18 01:22:44 +0300

    added cors headers

--- src/core/settings.py
+++ src/core/settings.py
@@ -41,12 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'base',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -165,3 +167,5 @@ REST_FRAMEWORK = {
         'base.backends.JWTBasicAuthentication',
     )
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
